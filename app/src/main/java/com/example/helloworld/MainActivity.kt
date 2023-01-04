80d77ce4728009dbb1b79eb9e91f0a7c21bc8b7c
Mike Pangas <mpangas@ufl.edu>
2023-01-04 11:52:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.helloButton)
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello to you too! Have a great day!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
